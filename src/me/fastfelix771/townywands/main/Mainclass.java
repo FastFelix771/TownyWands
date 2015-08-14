1c362fbc9a45c0458a24d4ccdaf58f8a313252fe
FastFelix771 <FastFelix771@Jarvis>
2015-08-14 09:09:51 +0200

    Some cleanup, the new code follows soon!

--- src/me/fastfelix771/townywands/main/Mainclass.java
+++ src/me/fastfelix771/townywands/main/Mainclass.java
@@ -1,79 +1,32 @@
 package me.fastfelix771.townywands.main;
 
 import java.io.File;
-import java.io.IOException;
 
-import me.fastfelix771.townywands.commands.CommandListener;
-import me.fastfelix771.townywands.inventory.InvAdmin;
-import me.fastfelix771.townywands.inventory.InvPlayer;
-import me.fastfelix771.townywands.inventory.InvResident;
-import me.fastfelix771.townywands.utils.Metrics;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
-public final class Mainclass extends JavaPlugin {
+public class Mainclass extends JavaPlugin {
 
 	private static Mainclass instance;
-	public static final ConsoleCommandSender console = Bukkit.getConsoleSender();
-	public static final PluginManager pm = Bukkit.getPluginManager();
-	public static final BukkitScheduler sh = Bukkit.getScheduler();
-	public static final Plugin towny = pm.getPlugin("Towny");
-	/**
-	 * @description You need to block my listeners if you want to your own guis, because TownyWands would listen on the inventory too and maybe cause trouble with your plugin...
-	 * @usage Set it to false and everything is okay :)
-	 */
-	public static boolean allowlisteners = true;
 
 	@Override
-	public final void onEnable() {
+	public void onEnable() {
 		instance = this;
-		loadMetrics();
-		setupGUIs();
-		console.sendMessage("§6[§3TownyWands§6]" + " §bEnabling...");
-		console.sendMessage("§6[§3TownyWands§6]" + " §aFound towny version §c" + towny.getDescription().getVersion() + " §a!");
-		getCommand("twa").setExecutor(new CommandListener());
-		getCommand("twu").setExecutor(new CommandListener());
-		pm.registerEvents(new PlayerHandler(), this);
-	}
-
-	private final void setupGUIs() {
-		InvPlayer.createGUI();
-		InvResident.createGUI();
-		InvAdmin.createGUI();
 	}
 
 	@Override
-	public final void onDisable() {
-		console.sendMessage("§6[§3TownyWands§6]" + " §bDisabling...");
+	public void onDisable() {
 		instance = null;
 	}
 
 	@Override
-	public final void onLoad() {
-		console.sendMessage("§6[§3TownyWands§6]" + " §bLoading...");
+	public void onLoad() {
 		saveDefaultConfig();
 		if (!new File(getDataFolder().getAbsolutePath() + "/messages.yml").exists()) {
 			saveResource("messages.yml", true);
 		}
 	}
 
-	public static final Mainclass getInstance() {
+	public static Mainclass getInstance() {
 		return instance;
-	}
-
-	private final void loadMetrics() {
-		if (getConfig().getBoolean("Metrics") == true) {
-			try {
-				final Metrics metrics = new Metrics(this);
-				metrics.start();
-			} catch (final IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
